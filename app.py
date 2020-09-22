044f164d1e6c0a5d972cba2843e28645bf97497c
John Naujoks <johnnaujoks@gmail.com>
2020-09-21 20:12:29 -0700

    Initial commit

---
+++ app.py
@@ -0,0 +1,20 @@
+from flask import Flask, jsonify
+from flask_cors import CORS
+
+DEBUG = True
+
+# Instantiate app
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+# enable CORS
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+# Sanity check route
+@app.route('/ping', methods=['GET'])
+def ping_pong():
+    return jsonify('PONG!')
+
+
+if __name__ = '__main__':
+    app.run()
